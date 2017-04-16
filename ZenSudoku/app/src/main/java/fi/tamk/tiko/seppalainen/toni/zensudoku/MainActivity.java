2bd1cb2d4b72eabeefe2135fb36a105a801591d1
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-17 01:23:06 +0300

    Add basic cell styling.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/MainActivity.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/MainActivity.java
@@ -3,13 +3,16 @@ package fi.tamk.tiko.seppalainen.toni.zensudoku;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TableLayout sudokuContainer;
+    private TextView selectedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +26,37 @@ public class MainActivity extends AppCompatActivity {
 
         for (int i = 1; i <= 81; i++) {
 
-            final TextView textView = new TextView(this);
-            textView.setText("" + ((i-1) % 9 + 1) );
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+            final TextView button = new TextView(this);
+            button.setText("" + ((i-1) % 9 + 1) );
+            button.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 30);
+            button.setBackgroundResource(R.drawable.cell);
+            button.setGravity(Gravity.CENTER);
+            row.addView(button);
 
-            row.addView(textView);
+            int pixels = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+            TableRow.LayoutParams params = (TableRow.LayoutParams) button.getLayoutParams();
+            params.width =  pixels;
+            params.height =  pixels;
+            button.setLayoutParams(params);
+
+            button.setOnClickListener(this);
+
             if (i % 9 == 0) {
                 // create new row
                 row = new TableRow(sudokuContainer.getContext());
                 sudokuContainer.addView(row);
             }
         }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (this.selectedButton != null) {
+            this.selectedButton.setBackgroundResource(R.drawable.cell);
+        }
+        this.selectedButton = (TextView) v;
+        v.setBackgroundResource(R.drawable.selected);
 
     }
 }

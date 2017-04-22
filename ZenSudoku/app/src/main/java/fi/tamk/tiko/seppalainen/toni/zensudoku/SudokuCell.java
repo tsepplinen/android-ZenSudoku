389f8efac5852de6e2875b9ac4a8bc79f5137543
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-22 19:36:06 +0300

    Add class SudokuCell.

---
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/SudokuCell.java
@@ -0,0 +1,21 @@
+package fi.tamk.tiko.seppalainen.toni.zensudoku;
+
+import android.content.Context;
+import android.util.TypedValue;
+import android.view.Gravity;
+
+/**
+ * Represents a cell in a sudoku grid.
+ */
+public class SudokuCell extends android.support.v7.widget.AppCompatTextView{
+
+    private final int TEXT_SIZE = 30;
+
+    public SudokuCell(Context context) {
+        super(context);
+
+        setTextSize(TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE);
+        setBackgroundResource(R.drawable.cell);
+        setGravity(Gravity.CENTER);
+    }
+}

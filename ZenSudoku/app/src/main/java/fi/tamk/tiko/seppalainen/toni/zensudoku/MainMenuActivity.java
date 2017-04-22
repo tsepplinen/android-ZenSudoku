54aef0174340a2a540dbec382c0084633596cb5e
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-22 19:09:02 +0300

    Add start play activity when from main menu.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/MainMenuActivity.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package fi.tamk.tiko.seppalainen.toni.zensudoku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +14,17 @@ public class MainMenuActivity extends AppCompatActivity {
     }
 
     public void handleButtonClick(View view) {
+        Difficulty difficulty = null;
         if (view.getId() == R.id.play_easy_button) {
-
+            difficulty = Difficulty.EASY;
         } else if (view.getId() == R.id.play_medium_button) {
-
+            difficulty = Difficulty.MEDIUM;
         } else if (view.getId() == R.id.play_medium_button) {
+            difficulty = Difficulty.HARD;
         }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("difficulty", difficulty);
+        startActivity(intent);
     }
 }

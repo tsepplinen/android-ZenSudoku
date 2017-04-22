5bccd35569322b663265191ee92f6f2a3838ddf3
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-22 18:09:25 +0300

    Add button handler stub to main menu.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/MainMenuActivity.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/MainMenuActivity.java
@@ -2,6 +2,7 @@ package fi.tamk.tiko.seppalainen.toni.zensudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void handleButtonClick(View view) {
+        if (view.getId() == R.id.play_button) {
+            System.out.println("MainMenuActivity.handleButtonClick play");
+        } else if (view.getId() == R.id.exit_button) {
+            System.out.println("MainMenuActivity.handleButtonClick exit");
+        }
     }
 }

2ffd57a1d119146e669ffeae2bbd7506fdf1d77c
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-23 17:28:43 +0300

    Add class SudokuCellGroup.

---
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/SudokuCellGroup.java
@@ -0,0 +1,24 @@
+package fi.tamk.tiko.seppalainen.toni.zensudoku;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tonis on 2017-04-23.
+ */
+
+class SudokuCellGroup {
+    private List<SudokuCell> cells;
+
+    public SudokuCellGroup() {
+        cells = new ArrayList<>();
+    }
+
+    public List<SudokuCell> getCells() {
+        return cells;
+    }
+
+    public void addCell(SudokuCell cell) {
+        cells.add(cell);
+    }
+}

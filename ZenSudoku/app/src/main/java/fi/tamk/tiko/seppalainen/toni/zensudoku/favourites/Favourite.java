e3069496a48eca05986fc497efc7a54239c5f2d7
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-05 14:33:31 +0300

    Add constructor to Favourite.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/favourites/Favourite.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/favourites/Favourite.java
@@ -8,4 +8,14 @@ import java.util.Date;
 class Favourite {
     public Date date;
     public String difficulty;
+
+    public Favourite(long time, int difficulty) {
+        this.date = new Date(time);
+        switch (difficulty) {
+            case 30: this.difficulty = "Hard"; break;
+            case 40: this.difficulty = "Medium"; break;
+            default:
+            case 50: this.difficulty = "Easy"; break;
+        }
+    }
 }

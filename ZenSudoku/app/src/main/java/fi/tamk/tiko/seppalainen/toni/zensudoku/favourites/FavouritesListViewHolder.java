5e945758b6baab9aa025fed205c98005c33648cf
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-05 13:21:47 +0300

    Add ViewHolder for favourites.

---
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/favourites/FavouritesListViewHolder.java
@@ -0,0 +1,13 @@
+package fi.tamk.tiko.seppalainen.toni.zensudoku.favourites;
+
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+
+/**
+ * Created by tonis on 2017-05-05.
+ */
+public class FavouritesListViewHolder extends ViewHolder {
+    public FavouritesListViewHolder(View itemView) {
+        super(itemView);
+    }
+}

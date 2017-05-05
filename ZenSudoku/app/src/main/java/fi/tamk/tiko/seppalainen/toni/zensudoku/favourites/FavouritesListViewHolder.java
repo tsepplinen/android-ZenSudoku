9e5b8346d128c077207169afc74e9f824bdfe8cd
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-05 14:04:40 +0300

    Add view fields to FavouriteListViewHolder.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/favourites/FavouritesListViewHolder.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/favourites/FavouritesListViewHolder.java
@@ -2,12 +2,21 @@ package fi.tamk.tiko.seppalainen.toni.zensudoku.favourites;
 
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.View;
+import android.widget.TextView;
+
+import fi.tamk.tiko.seppalainen.toni.zensudoku.R;
 
 /**
  * Created by tonis on 2017-05-05.
  */
 public class FavouritesListViewHolder extends ViewHolder {
+    public TextView dateView;
+    public TextView difficultyView;
+
     public FavouritesListViewHolder(View itemView) {
         super(itemView);
+
+        dateView = (TextView) itemView.findViewById(R.id.favouriteDate);
+        difficultyView = (TextView) itemView.findViewById(R.id.favouriteDifficulty);
     }
 }

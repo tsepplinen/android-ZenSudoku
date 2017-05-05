9e127c72da031058542677d60cd76b08b7b09b50
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-05 13:58:18 +0300

    Add class Favourite.

---
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/favourites/Favourite.java
@@ -0,0 +1,11 @@
+package fi.tamk.tiko.seppalainen.toni.zensudoku.favourites;
+
+import java.util.Date;
+
+/**
+ * Holds data for a single entry in favourites.
+ */
+class Favourite {
+    public Date date;
+    public String difficulty;
+}

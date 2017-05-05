46bab797bf287abd9b631565b6f9ff01c58005a5
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-05 13:22:12 +0300

    Add ListAdapter for favourites.

---
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/favourites/FavouritesListAdapter.java
@@ -0,0 +1,22 @@
+package fi.tamk.tiko.seppalainen.toni.zensudoku.favourites;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class FavouritesListAdapter extends RecyclerView.Adapter<FavouritesListViewHolder>{
+
+    @Override
+    public FavouritesListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(FavouritesListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}

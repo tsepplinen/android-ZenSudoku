fa1c19dd42f46c888cb48a8d5df3a2fdb95e4075
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-11 00:08:25 +0300

    Add javadoc to class WinActivity.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/WinActivity.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/WinActivity.java
@@ -5,6 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 
+/**
+ * Acts as a win screen for the puzzle.
+ *
+ * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
+ * @version 2017.0509
+ * @since 1.7
+ */
 public class WinActivity extends AppCompatActivity {
 
     @Override
@@ -27,6 +34,11 @@ public class WinActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Returns to previous view in the history stack.
+     *
+     * @param view Clicked View element.
+     */
     public void goBack(View view) {
         super.finish();
     }

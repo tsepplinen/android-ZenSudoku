405d7e8af0bea4835fa6799c75a7ae0c66110758
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-10 16:42:46 +0300

    Add javadoc to enum Difficulty.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/Difficulty.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/Difficulty.java
@@ -1,8 +1,21 @@
 package fi.tamk.tiko.seppalainen.toni.zensudoku;
 
+/**
+ * Represents the sudokus difficulty.
+ *
+ * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
+ * @version 2017.0509
+ * @since 1.7
+ */
 public enum Difficulty {
     EASY, MEDIUM, HARD;
 
+    /**
+     * Creates a Difficulty from given int value.
+     *
+     * @param value The value to create from.
+     * @return Created Difficulty object.
+     */
     public static Difficulty fromInt(int value) {
         switch (value) {
             case 40:

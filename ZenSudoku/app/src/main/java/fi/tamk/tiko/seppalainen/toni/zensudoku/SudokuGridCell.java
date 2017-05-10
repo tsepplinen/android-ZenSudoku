7fa1c1e991be953e24f6c99246d67465fb1cbb79
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-11 00:01:22 +0300

    Add javadoc to class SudokuGridCell.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/SudokuGridCell.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/SudokuGridCell.java
@@ -1,31 +1,96 @@
 package fi.tamk.tiko.seppalainen.toni.zensudoku;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Typeface;
 import android.util.TypedValue;
 import android.view.Gravity;
 
 /**
  * Represents a cell in a sudoku grid.
+ *
+ * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
+ * @version 2017.0509
+ * @since 1.7
  */
 public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView {
 
+    /**
+     * Text size for the number in the cell.
+     */
     private final int TEXT_SIZE = 30;
+
+    /**
+     * The cells X coordinate in the grid.
+     */
     private int cellX = 0;
+
+    /**
+     * The cells Y coordinate in the grid.
+     */
     private int cellY = 0;
+
+    /**
+     * The column group this cell belongs to.
+     */
     private SudokuCellGroup columnGroup;
+
+    /**
+     * The row group this cell belongs to.
+     */
     private SudokuCellGroup rowGroup;
+
+    /**
+     * The square group this cell belongs to.
+     */
     private SudokuCellGroup squareGroup;
+
+    /**
+     * The numeric value contained within this cell.
+     */
     private int value;
+
+    /**
+     * Tells if this cell should be highlighted.
+     */
     private boolean highlight;
+
+    /**
+     * Tells if this cell is selected.
+     */
     private boolean select;
+
+    /**
+     * Tells if this cell should be number highlighted.
+     */
     private boolean numberHighlight;
+
+    /**
+     * Tells if this cell has an initial value of the sudoku.
+     */
     private boolean isInitial = false;
+
+    /**
+     * The default graphic of this cell.
+     */
     private int defaultDrawable;
+
+    /**
+     * The default highlight graphic of this cell.
+     */
     private int defaultHighlight;
+
+    /**
+     * The default number highlight graphic of this cell.
+     */
     private int defaultNumberHighlight;
 
+    /**
+     * Creates a SudokuGridCell initialized with given parameters.
+     *
+     * @param parent The parent activity.
+     * @param x      X coordinate of the cell.
+     * @param y      Y coordinate of the cell.
+     */
     public SudokuGridCell(Activity parent, int x, int y) {
         super(parent);
         cellX = x;
@@ -33,6 +98,9 @@ public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView 
         setupStyle();
     }
 
+    /**
+     * Sets up this cell components visual style.
+     */
     private void setupStyle() {
         setTextSize(TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE);
         setGravity(Gravity.CENTER);
@@ -64,11 +132,14 @@ public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView 
             defaultHighlight = R.drawable.highlighted_border_right;
             defaultNumberHighlight = R.drawable.number_highlighted_border_right;
         }
-
-
         setBackgroundResource(defaultDrawable);
     }
 
+    /**
+     * Sets this cell to contain an initial value.
+     *
+     * @param initialValue The initial value this cell contains.
+     */
     public void setInitialValue(int initialValue) {
         this.value = initialValue;
         if (initialValue != 0) {
@@ -82,46 +153,61 @@ public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView 
         }
     }
 
+    /**
+     * Retrieves the cells X coordinate.
+     */
     public int getCellX() {
         return cellX;
     }
 
-    public void setCellX(int cellX) {
-        this.cellX = cellX;
-    }
-
+    /**
+     * Retrieves the cells Y coordinate.
+     */
     public int getCellY() {
         return cellY;
     }
 
-    public void setCellY(int cellY) {
-        this.cellY = cellY;
-    }
-
+    /**
+     * Sets the cells column group.
+     */
     public void setColumnGroup(SudokuCellGroup colGroup) {
         this.columnGroup = colGroup;
     }
 
+    /**
+     * Gets the cells column group.
+     */
     public SudokuCellGroup getColumnGroup() {
         return columnGroup;
     }
 
+    /**
+     * Sets the cells row group.
+     */
     public void setRowGroup(SudokuCellGroup rowGroup) {
         this.rowGroup = rowGroup;
     }
 
+    /**
+     * Gets the cells row group.
+     */
     public SudokuCellGroup getRowGroup() {
         return rowGroup;
     }
 
+    /**
+     * Sets the cells square group.
+     */
     public void setSquareGroup(SudokuCellGroup squareGroup) {
         this.squareGroup = squareGroup;
     }
 
-    public SudokuCellGroup getSquareGroup() {
-        return squareGroup;
-    }
 
+    /**
+     * Sets this cells value during game play.
+     *
+     * @param number The numeric value to set to this cell.
+     */
     public void setValue(int number) {
         if (!isInitial) {
             this.value = number;
@@ -134,10 +220,20 @@ public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView 
         }
     }
 
+    /**
+     * Retrieves the numeric value contained within this cell.
+     *
+     * @return The value of this cell.
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Sets this cells highlight state.
+     *
+     * @param shouldHighlight True if should highlight, false if not.
+     */
     public void setHighlight(boolean shouldHighlight) {
 
         if (highlight != shouldHighlight) {
@@ -146,6 +242,11 @@ public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView 
         }
     }
 
+    /**
+     * Sets this cells number highlight state.
+     *
+     * @param shouldHighlight True if should number highlight, false if not.
+     */
     public void setNumberHighlight(boolean shouldHighlight) {
 
         if (numberHighlight != shouldHighlight) {
@@ -154,6 +255,11 @@ public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView 
         }
     }
 
+    /**
+     * Sets this cells selection state.
+     *
+     * @param shouldSelect True if should be selected, false if not.
+     */
     public void setSelect(boolean shouldSelect) {
 
         if (select != shouldSelect) {
@@ -162,6 +268,9 @@ public class SudokuGridCell extends android.support.v7.widget.AppCompatTextView 
         }
     }
 
+    /**
+     * Updates the drawable according to highlight state.
+     */
     private void updateDrawable() {
 
         if (select) {

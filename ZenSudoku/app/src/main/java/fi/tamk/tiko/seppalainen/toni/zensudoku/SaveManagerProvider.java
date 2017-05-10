4b49ad8e0d7d9bebcc9f9aaa0269ec710d8cada6
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-10 23:30:44 +0300

    Add javadoc to class SaveManagerProvider.

--- ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/SaveManagerProvider.java
+++ ZenSudoku/app/src/main/java/fi/tamk/tiko/seppalainen/toni/zensudoku/SaveManagerProvider.java
@@ -1,19 +1,32 @@
 package fi.tamk.tiko.seppalainen.toni.zensudoku;
 
-import android.app.Activity;
-
 /**
  * Provides access to single instance of {@link SaveManager}.
+ *
+ * @author Toni Seppäläinen toni.seppalainen@cs.tamk.fi
+ * @version 2017.0509
+ * @since 1.7
  */
 public class SaveManagerProvider {
 
+    /**
+     * The instance of {@link SaveManager}
+     */
     private static SaveManager saveManager;
 
 
+    /**
+     * Initializes this class with the instance of {@link SaveManager}
+     */
     public static void init() {
         saveManager = new SaveManager();
     }
 
+    /**
+     * Retrieves the {@link SaveManager} instance.
+     *
+     * @return The handle to save manager.
+     */
     public static SaveManager getSaveManager() {
         return saveManager;
     }
